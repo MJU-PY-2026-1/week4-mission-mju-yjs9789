be9445fb16373897bcdd2f1cfc73c2e3bcb93d6e
yjs9789 <yjs9789@mju.ac.kr>
2026-03-31 01:48:20 +0000

    양준서 60231808 4차미션완료

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-# 파일이름 :
-# 작 성 자 :
+# 파일이름 :main.py
+# 작 성 자 :mju.3.31
+bucket_list=[]
+restaurant=input('맛집 리스트 입력:')
+bucket_list.append(restaurant)
+
+restaurant=input('맛집 리스트 입력:')
+bucket_list.append(restaurant)
+
+restaurant=input('맛집 리스트 입력:')
+bucket_list.append(restaurant)
+
+print(f"리스트: {bucket_list}")
+
+vip_restaurant=input('맛집 리스트 추가:')
+bucket_list.insert(0, restaurant)
+print(f'리스트: {bucket_list}')
+
+visited=input('도장 깨기:')
+bucket_list.remove(visited)
+print(f'리스트: {bucket_list}')
