f442b4f21270daa0a2036387469e871a8ec4d8e9
yjs9789 <yjs9789@mju.ac.kr>
2026-03-31 03:06:03 +0000

    1

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # 파일이름 :main.py
 # 작 성 자 :mju.3.31
 bucket_list=[]
-restaurant=input('맛집 리스트 입력: ')
+restaurant=input('맛집 리스트 입력:')
 bucket_list.append(restaurant)
 
-restaurant=input('맛집 리스트 입력: ')
+restaurant=input('맛집 리스트 입력:')
 bucket_list.append(restaurant)
 
-restaurant=input('맛집 리스트 입력: ')
+restaurant=input('맛집 리스트 입력:')
 bucket_list.append(restaurant)
 
 print(f"리스트: {bucket_list}")
 
-vip_restaurant=input('맛집 리스트 추가: ')
+vip_restaurant=input('맛집 리스트 추가:')
 bucket_list.insert(0, restaurant)
 print(f'리스트: {bucket_list}')
 
